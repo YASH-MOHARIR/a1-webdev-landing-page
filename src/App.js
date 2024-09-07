d2b04aa2fa8aad690b14829b02b27e3b9dd4a1c6
YASH-MOHARIR <68592074+YASH-MOHARIR@users.noreply.github.com>
2024-09-07 08:56:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yash Manish Moharir</h1>
+      <h2>Section : </h2>
+
+      <ul>
+        <li> Link to lab assignments :</li>
+        <li> Link to Kanabas :</li>
+        <li> Link to relevent repositories :</li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
