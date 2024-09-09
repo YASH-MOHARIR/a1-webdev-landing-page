1cf9cc21e943822e4b04774129207c09d236bd1c
YASH-MOHARIR <68592074+YASH-MOHARIR@users.noreply.github.com>
2024-09-09 07:57:00 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Yash Manish Moharir</h1>
-      <h2>Section : </h2>
+      <h2>Section : 	CS 5610, 03 </h2>
 
       <ul>
-        <li> Link to lab assignments :</li>
-        <li> Link to Kanabas :</li>
-        <li> Link to relevent repositories :</li>
+        <li> Link to lab assignments : (to be added)</li>
+        <li> Link to Kanabas : (to be added) </li>
+        <li> Link to relevent repositories : (to be added)</li>
       </ul>
       
     </div>
