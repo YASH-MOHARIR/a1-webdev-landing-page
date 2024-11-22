ab559ceee77c49aab97ee25bd69cd477ff554c3c
YASH-MOHARIR <68592074+YASH-MOHARIR@users.noreply.github.com>
2024-11-23 01:41:05 +0530

    Update PathParameters.tsx
    added divide multiply with path

--- src/Labs/A5/PathParameters.tsx
+++ src/Labs/A5/PathParameters.tsx
@@ -18,6 +18,17 @@ export default function PathParameters() {
          href={`${REMOTE_SERVER}/lab5/subtract/${a}/${b}`}>
          Substract {a} - {b}
       </a>
+      
+      <a className="btn btn-danger me-2" id="wd-path-parameter-subtract" 
+         href={${REMOTE_SERVER}/lab5/multiply/${a}/${b}}>
+         Multiply {a}*{b}
+      </a>
+
+      <a className="btn btn-danger me-2" id="wd-path-parameter-subtract" 
+         href={${REMOTE_SERVER}/lab5/divide/${a}/${b}}>
+         Divide {a}/{b}
+      </a>
+
       <hr />
     </div>
   );
